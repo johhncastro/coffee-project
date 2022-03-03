2ce044d2009dfe6ddc6c0e2257676ae51dc1a382
John Castro <johhncaastro@gmail.com>
2022-03-03 13:00:05 -0600

    add all option for selector

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function renderCoffee(coffee) {
     // html += '<td>' + coffee.id + '</td>';
     html += '<li>' + coffee.name + '</li>';
     html += '<li>' + coffee.roast + '</li>';
+    // html += '<li>' + coffee.type + '</li>';
     html += '</ul>';
 
     return html;
@@ -32,20 +33,20 @@ function updateCoffees(e) {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast:'light',type:'All'},
+    {id: 2, name: 'Half City', roast: 'light',type:'All'},
+    {id: 3, name: 'Cinnamon', roast: 'light',type:'All'},
+    {id: 4, name: 'City', roast: 'medium',type:'All'},
+    {id: 5, name: 'American', roast: 'medium',type:'All'},
+    {id: 6, name: 'Breakfast', roast: 'medium',type:'All'},
+    {id: 7, name: 'High', roast: 'dark',type:'All'},
+    {id: 8, name: 'Continental', roast: 'dark',type:'All'},
+    {id: 9, name: 'New Orleans', roast: 'dark',type:'All'},
+    {id: 10, name: 'European', roast: 'dark',type:'All'},
+    {id: 11, name: 'Espresso', roast: 'dark',type:'All'},
+    {id: 12, name: 'Viennese', roast: 'dark',type:'All'},
+    {id: 13, name: 'Italian', roast: 'dark',type:'All'},
+    {id: 14, name: 'French', roast: 'dark',type:'All'},
 ];
 
 var tbody = document.querySelector('#coffees');
